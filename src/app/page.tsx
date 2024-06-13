f25efe985b8b541a39c666381ad53d7c94af9bbe
tanto <tantows001@gmail.com>
2024-06-13 19:05:09 +0700

    feat: eager loading hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,14 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
+    <main className="scroll-smooth">
       <Navbar/>
 
       {/* Hero Section Start */}
       <section className="px-8 lg:px-24 md:px-16 pt-24 lg:pt-32 pb-16 max-w-7xl mx-auto">
         <div className="flex flex-wrap justify-center items-center">
           <div className="w-full lg:w-1/2 mb-6 lg:mb-0">
-            <Image src={'/mockup-zenspire.png'} alt="mockup-zenspire" className=" px-8" width={2000} height={500}></Image>
+            <Image src={'/mockup-zenspire.png'} alt="mockup-zenspire" loading="eager" className=" px-8" width={2000} height={500}></Image>
           </div>
           <div className="w-full lg:w-1/2 flex flex-col gap-6 lg:gap-8">
             <h1 className="text-center lg:text-left text-xl lg:text-3xl font-bold text-[#12719C] leading-8 lg:leading-10 mx-auto">Terapis Anda yang Terpersonalisasi untuk Dukungan Gangguan Kecemasan Sosial</h1>
@@ -27,7 +27,7 @@ export default function Home() {
       {/* Feature Section Start */}
       <section className="relative lg:-mt-8">
         <div className="slanted-bg absolute inset-0"></div>
-        <div className="px-8 lg:px-24 md:px-16 pt-16 pb-32 lg:pb-64 max-w-7xl mx-auto">
+        <div className="px-8 lg:px-24 md:px-16 pt-16 pb-32 lg:pb-64 max-w-7xl mx-auto" id="fitur">
           <div>
             <h4 className="text-center text-lg lg:text-xl font-bold text-[#1AAD90] mb-4">Fitur</h4>
             <h1 className="text-center text-xl lg:text-2xl font-bold text-[#12719C] mb-4">Apa yang dapat Zenspire tawarkan?</h1>
@@ -39,7 +39,7 @@ export default function Home() {
 
       {/* FAQ Section Start */}
       <section>
-        <div className="px-8 lg:px-24 md:px-16 py-16 max-w-7xl mx-auto">
+        <div className="px-8 lg:px-24 md:px-16 py-16 max-w-7xl mx-auto" id="faq">
           <h4 className="text-center text-lg lg:text-xl font-bold text-[#1AAD90] mb-4">FAQ</h4>
           <h1 className="text-center text-xl lg:text-2xl font-bold text-[#12719C] mb-4">Frequently Asked Questions</h1>
           <p className="text-center text-[#12719C] text-sm lg:text-md">Lihat untuk pertanyaan dan bantuan yang sering ditanyakan</p>
