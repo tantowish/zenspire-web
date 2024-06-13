b52545053444977ae8f99ee3ab03b0bbf72202af
tanto <tantows001@gmail.com>
2024-06-13 18:32:59 +0700

    feat: hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,51 @@
+import Navbar from "@/components/navbar";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <Navbar/>
+
+      {/* Hero Section Start */}
+      <section className="px-8 lg:px-24 md:px-16 pt-24 lg:pt-32 pb-16 max-w-7xl mx-auto">
+        <div className="flex flex-wrap justify-center items-center">
+          <div className="w-full lg:w-1/2 mb-6 lg:mb-0">
+            <Image src={'/mockup-zenspire.png'} alt="mockup-zenspire" className=" px-8" width={2000} height={500}></Image>
+          </div>
+          <div className="w-full lg:w-1/2 flex flex-col gap-6 lg:gap-8">
+            <h1 className="text-center lg:text-left text-xl lg:text-3xl font-bold text-[#12719C] leading-8 lg:leading-10 mx-auto">Terapis Anda yang Terpersonalisasi untuk Dukungan Gangguan Kecemasan Sosial</h1>
+            <p className="text-center lg:text-justify max-w-sm lg:max-w-none mx-auto text-[#12719C] text-sm lg:text-md">Zenspire memberikan perawatan Cognitive Behavioral Therapy yang terpersonalisasi dan dukungan personal. Zenspire menyediakan ruang terapi mandiri secara komprehensif. Dapat diakses kapanpun dan dimanapun tanpa biaya.</p>
+            <Link className='mx-auto lg:mx-0 px-4 py-2 bg-gradient-to-r from-[#2B9EA1] to-[#12719C] hover:from-[#12719C] hover:to-[#2B9EA1] text-sm transition-colors duration-300 rounded-full text-white w-fit' key={'unduh'} href={'#unduh'}>
+                Unduh Aplikasi
+            </Link>
+          </div>
         </div>
-      </div>
+      </section>
+      {/* Hero Section End */}
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      {/* Feature Section Start */}
+      <section className="relative lg:-mt-8">
+        <div className="slanted-bg absolute inset-0"></div>
+        <div className="px-8 lg:px-24 md:px-16 pt-16 pb-32 lg:pb-64 max-w-7xl mx-auto">
+          <div>
+            <h4 className="text-center text-lg lg:text-xl font-bold text-[#1AAD90] mb-4">Fitur</h4>
+            <h1 className="text-center text-xl lg:text-2xl font-bold text-[#12719C] mb-4">Apa yang dapat Zenspire tawarkan?</h1>
+            <p className="text-center text-[#12719C] text-sm lg:text-md">Cari tau apa yang akan Anda dapatkan dengan Zenspire</p>
+          </div>
+        </div>
+      </section>
+      {/* Feature Section End */}
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      {/* FAQ Section Start */}
+      <section>
+        <div className="px-8 lg:px-24 md:px-16 py-16 max-w-7xl mx-auto">
+          <h4 className="text-center text-lg lg:text-xl font-bold text-[#1AAD90] mb-4">FAQ</h4>
+          <h1 className="text-center text-xl lg:text-2xl font-bold text-[#12719C] mb-4">Frequently Asked Questions</h1>
+          <p className="text-center text-[#12719C] text-sm lg:text-md">Lihat untuk pertanyaan dan bantuan yang sering ditanyakan</p>
+        </div>
+      </section>
+      {/* FAQ Section End */}
     </main>
   );
 }
---
+++ src/components/navbar.tsx
@@ -0,0 +1,114 @@
+"use client"
+
+import Link from 'next/link'
+import Image from 'next/image'
+import { usePathname } from 'next/navigation'
+import React, { useEffect, useRef, useState } from 'react'
+
+const Navbar = () => {
+  const [isRouting, setisRouting] = useState(false)
+  const path = usePathname()
+  const [prevPath, setPrevPath] = useState("/")
+  const [isActive, setIsActive] = useState(false)
+
+  const handleHamburgerClick = ()=>{
+    setIsActive(!isActive)
+  }
+
+  useEffect(()=>{
+      if(prevPath !== path){
+        setIsActive(false);
+        setisRouting(true)
+      }
+  }, [path, prevPath])
+
+  useEffect(()=>{
+      if(isRouting){
+          setPrevPath(path)
+          const timeout = setTimeout(()=>{
+              setisRouting(false)
+          }, 1200)
+
+          return()=>clearTimeout(timeout)
+      }
+  }, [isRouting])
+
+  const NavLinks = [
+      {
+        link: "beranda",
+        name:"Beranda"
+      },
+      {
+        link: "fitur",
+        name:"Fitur",
+      },
+      {
+        link: "faq",
+        name:"FAQ"
+      },
+      {
+        link: "kontak",
+        name:"Kontak"
+      },
+
+  ]
+  return (
+    <nav className='navbar-fixed top-0 w-full z-[10] relative'>
+        <div className='flex flex-wrap justify-between max-w-7xl mx-auto px-8 lg:px-24 md:px-16 py-4 items-center'>
+            <div>
+                <Link
+                key="beranda"
+                href="#beranda"
+                className='flex flex-wrap gap-4'
+                >
+                    <Image
+                    className='w-6 lg:w-8'
+                    src="/zenspire-logo.png"
+                    alt='zenspire-logo'
+                    width={288}
+                    height={162}
+                    />
+                    <p className='text-[#12719C] font-bold text-xl lg:text-2xl'>Zenspire</p>
+                </Link> 
+            </div>
+            <div className='hidden md:flex flex-wrap gap-12'>
+                {isRouting}
+                {NavLinks.map((nav)=>(
+                    <Link
+                    key={nav.name}
+                    className={`text-[#12719C] hover:font-semibold hover:underline hover:underline-offset-8 transition-all ease-in-out`}
+                    href={`#${nav.link}`}>{nav.name}</Link>
+                ))}
+            </div>
+            <div className='flex items-center px-4 md:hidden'>
+                <button id='hamburger' name='hamburger' type='button' className={`block absolute right-4`} onClick={handleHamburgerClick}>
+                    <span className='w-[30px] h-[2px] my-2 block bg-[#12719C] transition duration-500 ease-out origin-top-left'></span>
+                    <span className='w-[30px] h-[2px] my-2 block bg-[#12719C] transition duration-500 ease-out'></span>
+                    <span className='w-[30px] h-[2px] my-2 block bg-[#12719C] transition duration-500 ease-out origin-bottom-left'></span>
+                </button>
+                <div className={`nav-menu absolute z-[10] py-5 rounded-b-3xl w-full right-0 top-full ${!isActive ? 'hidden' : ''}`}>
+                    <div className='flex flex-col gap-3 px-8'>
+                        {isRouting}
+                        {NavLinks.map((nav)=>(
+                        <Link
+                        key={nav.name}
+                        className={`text-[#12719C] hover:font-semibold hover:underline hover:underline-offset-4 transition-all ease-in-out`}
+                        href={`#${nav.link}`}>{nav.name}</Link>
+                        ))}
+                        <Link className='px-4 py-2 bg-[#12719C] rounded-full text-white hover:bg-[#12709cd7] transition-all text-sm w-fit' key={'unduh'} href={'#unduh'}>
+                            Unduh Aplikasi
+                        </Link>
+                    </div>
+                </div>
+            </div>
+            <div className='hidden md:block'>
+                <Link className='px-4 py-2 bg-[#12719C] rounded-full text-white hover:bg-[#12709cd7] transition-all text-sm' key={'unduh'} href={'#unduh'}>
+                    Unduh Aplikasi
+                </Link>
+            </div>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
